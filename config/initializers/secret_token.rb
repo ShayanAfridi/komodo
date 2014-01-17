5e364b8c5e7f13d979c94b760df314bd1f372906
Shayan <palmtreetutoring@gmail.com>
2014-01-17 14:25:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Komodo::Application.config.secret_key_base = '2d014b1cbad49676d5619d3b4a3973a1ed767d4a0704238809d17606b2ef817cc7445172e10ec3846e6c24000884ff90dc1b6da7071047639387a44b6ee6e4fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Komodo::Application.config.session_store :cookie_store, key: '_komodo_session'
